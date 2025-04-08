cf05d8decfeb8c6b2b6f1ceab63f72d8d9a393af
dariahnn <dariananandaz@gmail.com>
2025-04-08 16:56:33 +0300

    another

--- src/oop/clubentry.kt
+++ src/oop/clubentry.kt
@@ -33,3 +33,130 @@ interface ClubRules{
     fun canEnter(member:Member) : Boolean
 }
 // Main class
+class NightClub(override val minimumAge: Int,
+                override val vipBenefits: String)
+    : ClubRules{
+        private val currentMembers = mutableListOf<Member>()
+        private val bannedMembers = mutableListOf<String>()
+    companion object {
+        val timeFormatter: DateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
+    }
+
+    override fun canEnter(member: Member): Boolean {
+        return member.age >= minimumAge && !bannedMembers.contains(member.id)
+    }
+    fun addMember(member: Member){
+        // validating if the member can enter my club
+        if(canEnter(member)){
+            // add our member to our list of members
+            currentMembers.add(member)
+            println("${member.name} (${member.membership})entered at ${member.entryTime.format(timeFormatter)}")
+            // check on the benefits if member is VIP, REGULAR , GUEST
+
+        }else{
+            println("${member.name} cannot enter." +
+                    "${if(member.age < minimumAge){
+                            "too young"
+                    } else{ 
+                            "banned"
+                    }
+                    }")
+        }
+    }
+    // this function will allow my objecs to be entered in the banned section listing
+    fun banMember(id : String){
+        // adding id to my banned members list
+        bannedMembers.add(id)
+        //removing the member from the current members listing
+        currentMembers.removeIf{it.id == id}
+        println("\nMember $id has been banned")
+    }
+    fun listCurrentMembers(){
+        if(currentMembers.isEmpty()){
+            println("\nNo members currently added")
+            return
+        }
+        println("\nCurrent members in club :")
+        currentMembers.forEach{
+            println("-${it.name} (${it.membership}) since ${it.entryTime.format(timeFormatter)}")
+        }
+    }
+
+}
+
+// MY TERMINAL APP
+//input helper functions
+fun readString(prompt: String) : String{
+    print(prompt)
+    return readlnOrNull()?.trim() ?: ""
+}
+fun readInt(prompt: String) : Int {
+    while(true){
+        try{
+            print(prompt)
+            return readlnOrNull()?.toInt() ?: throw NumberFormatException()
+        }catch (e:NumberFormatException){
+            println("please enter a valid number")
+        }
+    }
+}
+// display our menu
+fun showMenu(){
+    println("\n=== CLUB MANAGEMENT SYSTEM")
+    println("\n1. Add New Member")
+    println("\n2. Ban Member")
+    println("\n3. List Current Member")
+    println("\n4. Exit")
+    print("Enter your choice: ")
+}
+fun main(){
+    // create a club
+    val  club = NightClub(
+        minimumAge = 18
+        vipBenefits = "Free drinks and lounge access"
+        name = "XoXo"
+    )
+    println("WELCOME TO THE CLUB MANAGEMENT SYSTEM")
+    while (true){
+        showMenu()
+        when (readlnOrNull()?.trim()){
+            "1" -> {
+                println("\nAdd new member")
+                val id = readString("Enter Member id: ")
+                val name = readString("Enter Member name: ")
+                val age = readString("Enter Member age: ")
+                var membership: MembershipTypes? = null
+                while (membership == null){
+                    print("Enter membership type (R:Regular, V:VIP ,G:Guest)")
+                    membership = MembershipTypes.fromInput(
+                        readlnOrNull()?.trim() ?: ""
+                    )
+                    if(membership == null){
+                        println("Invalid membership type")
+                    }
+                }
+                club.addMember(Member(id, name , age , membership))
+            }
+            "2" -> {
+                println("\nBan Member")
+                val id = readString("Enter Member ID to ban")
+                club.banMember(id)
+            }
+            "3" -> {
+                club.listCurrentMembers()
+            }
+            "4" -> {
+                println("Exiting program")
+                return
+            }
+            else -> {
+                println("Invalid choice selected.")
+            }
+        }
+    }
+
+}
+
+
+
+
